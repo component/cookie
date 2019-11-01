f2f087346cdf9ba533a50144cd3c9adb3e095d39
Nathan Friedly <nathan@fullstory.com>
2019-11-01 13:02:27 -0400

    fix handling of = in cookie values

--- index.js
+++ index.js
@@ -98,10 +98,18 @@ function parse(str) {
   var obj = {};
   var pairs = str.split(/ *; */);
   var pair;
-  if ('' == pairs[0]) return obj;
+  var eqidx;
+  var name;
+  var value;
   for (var i = 0; i < pairs.length; ++i) {
-    pair = pairs[i].split('=');
-    obj[decode(pair[0])] = decode(pair[1]);
+    pair = pairs[i];
+    eqidx = pair.indexOf("=");
+    if (eqidx === -1) {
+      eqidx = pair.length;
+    }
+    name = decode(pair.substr(0, eqidx));
+    value = decode(pair.substr(eqidx + 1)); // +1 because we don't want the =
+    obj[name] = value;
   }
   return obj;
 }
